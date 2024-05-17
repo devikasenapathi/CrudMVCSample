e7e63c8a14c0e302e75bb30c030a6cd784ab1054
devika <senapathidevika@gmail.com>
2024-05-17 14:40:26 +0530

    Add project files.

---
+++ CrudMVCSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CrudMVCSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
